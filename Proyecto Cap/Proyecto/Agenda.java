060926999dd3c54bd0d7e1f2fab8de1325d0513b
AWLosT <luiiis4@hotmail.com>
2017-02-12 10:33:41 -0600

    Add files via upload
    Faltan los metodos de borrar en Agenda.
    Se está tomando contacto y es lo unico que se está imprimiendo y pidiendo, falta especificar el tipo de contacto.(Doctor,Estudiante, persona, Licenciado).

--- Proyecto Cap/Proyecto/Agenda.java
+++ Proyecto Cap/Proyecto/Agenda.java
@@ -1,7 +1,10 @@
 package Proyecto;
 import java.io.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Agenda {
+	static String dato;
 Contacto[]lista = new Contacto[100];//Se crea una nueva lista de tipo contacto
 private int contador=0;//Contador de objetos
 //VALIDACIONES
@@ -22,6 +25,7 @@ if (contador<100) {
 	this.lista[contador].Set_ApellidoM(apellidoM);
 	this.lista[contador].Set_MediosCon(mediosCon);
 	this.contador++;
+	ordenar();
 }
 else {
 	System.out.println("La agenda está llena");
@@ -63,24 +67,100 @@ public void ordenar()//Se usa el metodo burbuja
 //ELIMINAR TODOS LOS CONTACTOS
 public void eliminart()
 {
-try {
-	
-} catch (Exception e) {
 
-}	
 }
 //ELIMINAR
 public void eliminar()
 {
+	boolean encontrado=false;
+	System.out.println("Nombre del contacto a eliminar:");
+	pedirdatos();
+	String eliminar = dato;
+	System.out.println("Apellido Paterno del contacto a eliminar:");
+	pedirdatos();
+	String eliminar1 = dato;
+	if (contador==0) {
+		System.out.println("No hay contactos.");
+	} else {
+		for (int i = 0; i < contador; i++) {
+			if ((eliminar.equalsIgnoreCase((this.lista[i].Get_Nombre()))&&(eliminar1.equalsIgnoreCase(this.lista[i].Get_ApellidoP())))) {
+				System.out.println(i+1+". "+this.lista[i].Get_Nombre() +" " +this.lista[i].Get_ApellidoP() + ": " + this.lista[i].Get_MediosCon());
+			    encontrado=true;
+			}
+		}
+	if(encontrado)
+	{
+		System.out.println("Que contacto quieres eliminar, introduce algún medio de contacto asociado.");
+		pedirdatos();
+		String medio = dato;//Aqui falta comparar el medio y eliminar el contacto si el medio es igual.
+	
+	}
+	}
 	
 }
 //MODIFICAR
 public void modificar() {
-	try {
-		
-	} catch (Exception e) {
-		
+boolean encontrado=false;
+System.out.println("Nombre del contacto a modificar:");
+pedirdatos();
+String modificar = dato;
+System.out.println("Apellido Paterno del contacto a modificar:");
+pedirdatos();
+String modificar1 = dato;
+if (contador==0) {
+	System.out.println("No hay contactos.");
+} else {
+	for (int i = 0; i < contador; i++) {
+		if ((modificar.equalsIgnoreCase((this.lista[i].Get_Nombre()))&&(modificar1.equalsIgnoreCase(this.lista[i].Get_ApellidoP())))) {
+			System.out.println(i+1+". "+this.lista[i].Get_Nombre() +" " +this.lista[i].Get_ApellidoP() + ": " + this.lista[i].Get_MediosCon());
+		    encontrado=true;
+		}
+	
+	if(encontrado)//Falta poner condicionales para todos y poner dependiendo el tipo de persona.
+	{
+		System.out.println("Que es lo que quieres modificar?: introduce 1: para el nombre, 2: para el apellido paterno, 3: para el medio de contacto");
+        pedirdatos();
+        String modifi = dato;
+        switch (modifi) {
+		case "1":
+			System.out.println("Dame el nuevo nombre");
+			pedirdatos();
+			String nombrenuevo = dato;
+			this.lista[i].Set_Nombre(nombrenuevo);
+			break;
+
+		case "2":
+			System.out.println("Dame el nuevo apellido paterno");
+			pedirdatos();
+			String nuevoapp = dato;
+			this.lista[i].Set_Nombre(nuevoapp);
+			break;
+			
+		case "3":
+			System.out.println("Dame el nuevo apellido materno");
+			pedirdatos();
+			String nuevoapm = dato;
+			this.lista[i].Set_Nombre(nuevoapm);
+			break;
+			
+		default:
+			System.out.println("Introduce un valor correcto");
+			break;
+		}
 	}
+	else{System.out.println("No hay contactos similares.");}
+}
+}
 }
 
+public static void pedirdatos()
+{
+	try {
+BufferedReader dato1 = new BufferedReader(new InputStreamReader(System.in));
+	dato= dato1.readLine().toUpperCase();
+} catch (IOException e) {
+	Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, e);
+	e.printStackTrace();
+}
+}
 }
